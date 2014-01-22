6d4fca3b865ffc8053d73a9a664b441783a79db0
Maximilian Böhm <mb@xabe.de>
2014-01-22 10:15:03 +0300

    up

--- GJU_CS_TcpRsaAes/src/start/client/SecureTCPClient.java
+++ GJU_CS_TcpRsaAes/src/start/client/SecureTCPClient.java
@@ -1,10 +1,28 @@
 package start.client;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.Socket;
+import java.security.SecureRandom;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class SecureTCPClient{
    
- public String send(String sMessage){
+   public String handleSecureMessage(String sMessage) {
+      String sRSAPublicKey = send("REQ_RSA_PUB_KEY");
+      System.out.println(sRSAPublicKey);
+      // generate AES key
+      // send AES key encrypted by RSA
+      // send message encrypted by AES
+      
+      return "DEI_MUDDA";
+      
+   }
+   
+ private String send(String sMessage){
     try {
       
      String sentence;
@@ -26,5 +44,18 @@ public class SecureTCPClient{
        e.printStackTrace();
        return "FEHLER";
     }
+    
+    
  }
+ 
+public void getAES() {
+   try {
+      KeyGenerator keygen = KeyGenerator.getInstance("AES");
+      SecureRandom random = new SecureRandom();
+      keygen.init(random);
+      SecretKey key = keygen.generateKey();
+   } catch (Exception e) {
+      // TODO: handle exception
+   }
+}
 }
--- GJU_CS_TcpRsaAes/src/start/server/SecuredServer.java
+++ GJU_CS_TcpRsaAes/src/start/server/SecuredServer.java
@@ -1,10 +1,38 @@
 package start.server;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 
-public class SecuredServer
-{
+public class SecuredServer{
+   
+   
+   
    public static void main(String argv[]) throws Exception{
+      new SecuredServer();
+   }
+   
+   
+   // members
+   Key publicKey = null;
+   Key privateKey = null;
+   
+   
+   
+   public SecuredServer() {
+      // init
+      
+      initServer();
+      // start
+      start();
+   }
+   private void start() {
+      try {
+         
          String clientSentence;
          String capitalizedSentence;
 
@@ -14,10 +42,50 @@ public class SecuredServer
                BufferedReader   inFromClient = new BufferedReader  (new InputStreamReader(connectionSocket.getInputStream()));
                DataOutputStream outToClient  = new DataOutputStream(connectionSocket.getOutputStream());
                clientSentence = inFromClient.readLine();
-               System.out.println("Received: " + clientSentence);
-               capitalizedSentence = clientSentence.toUpperCase() + '\n';
+               
+               
+               
+               capitalizedSentence = handleRequest(clientSentence) + '\n';
                outToClient.writeBytes(capitalizedSentence);
             }
          } 
+      } catch (Exception e) {
+         e.printStackTrace();
       }
+   }
+   
+   private String handleRequest(String sRequest){
+      if(sRequest.startsWith("REQ_RSA_PUB_KEY"))
+         return new String(publicKey.getEncoded());
+      else if(sRequest.startsWith("REQ_AES_KEY"))
+         return retrieveAESKeyByRSACipher(sRequest);
+      else if(sRequest.startsWith("REQ_AES_CONTENT"))
+         return encryptAndDescryptRequest(sRequest);
+      else
+         return "ERROR";
+   }
+   
+   public String encryptAndDescryptRequest(String sReq) {
+      return sReq;
+   }
+   
+   public String retrieveAESKeyByRSACipher(String sReq){
+      return sReq;
+   }
+   
+   
+   private void initServer() {
+      try {
+         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+         kpg.initialize(2048);
+         KeyPair kp = kpg.genKeyPair();
+         publicKey = kp.getPublic();
+         privateKey = kp.getPrivate();
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   
 } 
