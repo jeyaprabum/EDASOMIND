e3cfe14e4be69e032ae54e8eb06f55a72e68a4e2
Maximilian Böhm <mb@xabe.de>
2014-04-10 12:18:15 +0200

    up

--- MongoTestCase/src/main/java/com/maximilianboehm/hsregensburg/bachelor/Action.java
+++ MongoTestCase/src/main/java/com/maximilianboehm/hsregensburg/bachelor/Action.java
@@ -3,15 +3,15 @@ package com.maximilianboehm.hsregensburg.bachelor;
 import com.google.code.morphia.Datastore;
 
 public class Action {
-   
+
    public void doIt(Datastore ds) {
       ds.save(new Employee("Mister", "GOD", null, 0));
-      
+
       for (Employee e :ds.find(Employee.class))
-         System.out.println(e.firstName+" "+e.lastName+" "+e.salary);
-      
-//      ds.delete(ds.createQuery(Employee.class));
-      
+         System.out.println(e.lastName+" "+e.salary);
+
+      //      ds.delete(ds.createQuery(Employee.class));
+
    }
 
 }
