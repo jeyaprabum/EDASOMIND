b6c4096103fc114c318b6ca9d79c96a56c117cab
Maximilian Böhm <mb@xabe.de>
2014-06-03 12:07:45 +0200

    init. add name

---
+++ MongoTestCase/src/main/java/com/maximilianboehm/hsregensburg/bachelor/errorcases/ReintroduceAttribute.java
@@ -0,0 +1,10 @@
+package com.maximilianboehm.hsregensburg.bachelor.errorcases;
+
+import com.google.code.morphia.annotations.Entity;
+
+@Entity
+public class ReintroduceAttribute {
+
+	private String name;
+	
+}
