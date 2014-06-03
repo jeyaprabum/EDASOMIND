ea09c1ee619f6ecce2326eed2b99b95949d62baf
Maximilian Böhm <mb@xabe.de>
2014-06-03 12:14:37 +0200

    up

---
+++ MongoTestCase/src/main/java/com/maximilianboehm/hsregensburg/bachelor/errorcases/DeleteAttribute.java
@@ -0,0 +1,11 @@
+package com.maximilianboehm.hsregensburg.bachelor.errorcases;
+
+import com.google.code.morphia.annotations.Entity;
+
+@Entity
+public class DeleteAttribute {
+
+	private String firstname;
+	private String lastname;
+	
+}
