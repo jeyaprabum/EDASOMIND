77957f8095d0bf0cb17bfd34fb706527880b692d
Maximilian Böhm <mb@xabe.de>
2014-07-14 21:16:27 +0200

    up

--- MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/Test.java
+++ MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/Test.java
@@ -1,5 +1,11 @@
 package com.maximilian_boehm.hsregensburg.bachelor;
 
+import com.google.code.morphia.annotations.Entity;
+
+@Entity
 public class Test {
+
+    private String Test;
+    private String ABC;
 
 }
