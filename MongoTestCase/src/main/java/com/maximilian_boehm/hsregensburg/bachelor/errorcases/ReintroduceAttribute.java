0f5224d1bf0ac3dc210d79ffc8ecbab25e31e278
Maximilian Böhm <mb@xabe.de>
2014-07-14 16:08:47 +0200

    add again

--- MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/errorcases/ReintroduceAttribute.java
+++ MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/errorcases/ReintroduceAttribute.java
@@ -8,6 +8,7 @@ public class ReintroduceAttribute {
     private String lastname;
     private String firstname;
 
-
+    // ERROR: Reintroduce name which was already existent but previously not listened any more
+    private String name;
 
 }
