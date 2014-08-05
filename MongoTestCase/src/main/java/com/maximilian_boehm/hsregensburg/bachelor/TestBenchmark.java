063f81623c15b891edef0157d177b35babf8d018
Maximilian Böhm <mb@xabe.de>
2014-08-05 17:21:14 +0200

    1

---
+++ MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/TestBenchmark.java
@@ -0,0 +1,20 @@
+package com.maximilian_boehm.hsregensburg.bachelor;
+
+import com.google.code.morphia.annotations.AlsoLoad;
+import com.google.code.morphia.annotations.NotSaved;
+import com.google.code.morphia.annotations.Transient;
+
+public class TestBenchmark {
+
+    @Transient
+    private String ABC;
+
+    @NotSaved
+    private long DEF;
+
+    @AlsoLoad("ABC")
+    private String GHI;
+
+
+
+}
