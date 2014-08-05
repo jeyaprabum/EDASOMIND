42b9e437675631aa73342de143c0a4c67c1e2ec9
Maximilian Böhm <mb@xabe.de>
2014-08-05 17:36:35 +0200

    start

---
+++ MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/benchmark/TestHighNumberVar.java
@@ -0,0 +1,65 @@
+package com.maximilian_boehm.hsregensburg.bachelor.benchmark;
+
+import com.google.code.morphia.annotations.Entity;
+
+@Entity
+public class TestHighNumberVar {
+
+    private long l0;
+    private long l1;
+    private long l2;
+    private long l3;
+    private long l4;
+    private long l5;
+    private long l6;
+    private long l7;
+    private long l8;
+    private long l9;
+    private long l10;
+    private long l11;
+    private long l12;
+    private long l13;
+    private long l14;
+    private long l15;
+    private long l16;
+    private long l17;
+    private long l18;
+    private long l19;
+    private long l20;
+    private long l21;
+    private long l22;
+    private long l23;
+    private long l24;
+    private long l25;
+    private long l26;
+    private long l27;
+    private long l28;
+    private long l29;
+    private long l30;
+    private long l31;
+    private long l32;
+    private long l33;
+    private long l34;
+    private long l35;
+    private long l36;
+    private long l37;
+    private long l38;
+    private long l39;
+    private long l40;
+    private long l41;
+    private long l42;
+    private long l43;
+    private long l44;
+    private long l45;
+    private long l46;
+    private long l47;
+    private long l48;
+    private long l49;
+
+
+
+
+
+
+
+}
