fab5d2dbcab3065e00084eacd70303a243a80f13
Maximilian Böhm <mb@xabe.de>
2014-08-05 17:40:44 +0200

    a

---
+++ MongoTestCase/src/main/java/com/maximilian_boehm/hsregensburg/bachelor/benchmark/HighNumberCommit.java
@@ -0,0 +1,23 @@
+package com.maximilian_boehm.hsregensburg.bachelor.benchmark;
+
+import com.google.code.morphia.annotations.Entity;
+
+@Entity
+public class HighNumberCommit {
+
+    private String s0;
+    private String s1;
+    private String s2;
+    private String s3;
+
+    private Double d0;
+    private Double d1;
+    private Double d2;
+    private Double d3;
+
+
+    private Float f0;
+    private Float f1;
+    private Float f2;
+    private Float f3;
+}
